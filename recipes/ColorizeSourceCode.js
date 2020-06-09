8cf6c0753bba2fd97ff37dc11901f00071c57a39
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-09 11:21:31 -0300

    Apply colors to strings

--- recipes/ColorizeSourceCode.js
+++ recipes/ColorizeSourceCode.js
@@ -8,4 +8,7 @@ const text = files.read('sourceCode.html')
 
 const codeRegex = /<code>[\s\S]*<\/code>/i
 let code = text.match(codeRegex)[0]
+
+// Strings...
+code = applyColor(code, /(\".*\")/g, 'ce9178')
 console.log(code)
