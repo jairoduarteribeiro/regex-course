04469f1cef2f32399a22d121166df4a908584724
Jairo Ribeiro <jairoduarte123@gmail.com>
2020-06-09 12:09:44 -0300

    Get numbers from 0 to 249

--- recipes/Intervals.js
+++ recipes/Intervals.js
@@ -1,4 +1,4 @@
 const text = '0 1 10 192 199 201 249 255 256 312 1010 1512'
 
 // numbers from 0 to 255
-console.log(text.match(/\b(\d{1,2}|1\d{2})\b/g))
+console.log(text.match(/\b(\d{1,2}|1\d{2}|2[0-4]\d)\b/g))
